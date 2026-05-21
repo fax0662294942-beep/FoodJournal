de7c2c24df62737c940e7ecf4b97719b4dba7b49
fax0662294942-beep <fax0662294942@gmail.com>
2026-05-22 00:02:58 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE = 'foodjournal-v100';
+const CDN = [
+  'https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.0/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore-compat.js',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CDN).catch(()=>{})));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  // CDN: cache-first
+  if(CDN.some(u=>url.startsWith(u.split('/').slice(0,3).join('/')))) {
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+    return;
+  }
+  // HTML: network-first
+  if(e.request.mode==='navigate') {
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(fetch(e.request));
+});
+
+self.addEventListener('message', e => {
+  if(e.data==='SKIP_WAITING') self.skipWaiting();
+});
